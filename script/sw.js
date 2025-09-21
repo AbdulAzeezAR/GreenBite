2232f5ab04e2c689d34d443418fd5c3305634e52
AbdulAzeezAR <seilxng@outlook.com>
2025-09-21 21:34:49 +0530

    Update website

--- script/sw.js
+++ script/sw.js
@@ -1,14 +1,18 @@
 const CACHE_NAME = 'greenbite-v1';
 const urlsToCache = [
-  '/',
-  '/styles/styles.css',
-  '/script.js',
-  '/index.html',
-  '/recipes.html',
-  '/calculator.html',
-  '/workout.html',
-  '/mindfulness.html',
-  '/contact.html'
+  './',
+  './styles/styles.css',
+  './index.html',
+  './recipes.html',
+  './calculator.html',
+  './workout.html',
+  './mindfulness.html',
+  './contact.html',
+  './favicon_io/favicon.ico',
+  './favicon_io/android-chrome-192x192.png',
+  './favicon_io/android-chrome-512x512.png',
+  './json/manifest.json',
+  './script/script.js'
 ];
 
 self.addEventListener('install', event => {
